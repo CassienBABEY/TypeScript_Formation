01bc9883a3609e50fea74a07917c361bbd8d4ad4
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 18:07:29 +0200

    7_9

---
+++ 7_Les_Generics_Utility_Types/7_9_G_Exclude.js
@@ -0,0 +1,18 @@
+"use strict";
+// type Easing = "ease-in" | "ease-out" | "ease-in-out"
+// type Animate = Exclude<Easing, "ease-out">
+// class UIElement {
+//     animate(x: number, y: number, easing: Animate) {
+//         if( easing === "ease-in" ) {
+//             console.log("ease-in")
+//         }
+//         if( easing === "ease-out" ) {
+//         console.log("ease-out")
+//         } // Erreur dû au Exclude 
+//         if (easing === "ease-in-out") {
+//         console.log("ease-in-out")
+//         }
+//     }
+// }
+// let button = new UIElement();
+// button.animate(0, 0, "ease-in");
