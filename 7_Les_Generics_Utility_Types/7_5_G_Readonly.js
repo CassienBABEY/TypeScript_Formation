17338ec4a42eb26d792d1a2a423da256743bed53
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 16:36:21 +0200

    7_5

---
+++ 7_Les_Generics_Utility_Types/7_5_G_Readonly.js
@@ -0,0 +1,16 @@
+"use strict";
+// interface Todo {
+//     title: string;
+//     description: string;
+// }
+// const myTodo: Readonly<Todo> = {
+//     title: "Sport",
+//     description: "Go to the gym"
+// }
+// console.log(myTodo);
+// myTodo.description = "Go to the park";
+let colors = ["blue", "red", "green", "yellow"];
+colors.push("purple");
+console.log(colors);
+let colors2 = ["blue", "red", "green", "yellow"];
+colors2.push("purple"); // Readonly
---
+++ 7_Les_Generics_Utility_Types/7_5_G_Readonly.ts
@@ -0,0 +1,21 @@
+// interface Todo {
+//     title: string;
+//     description: string;
+// }
+
+// const myTodo: Readonly<Todo> = {
+//     title: "Sport",
+//     description: "Go to the gym"
+// }
+
+// console.log(myTodo);
+// myTodo.description = "Go to the park";
+
+
+let colors = ["blue", "red", "green", "yellow"]
+colors.push("purple")
+console.log(colors);
+
+let colors2: Readonly<string[]> = ["blue", "red", "green", "yellow"]
+colors2.push("purple") // Readonly
+
