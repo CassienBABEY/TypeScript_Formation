8ba83dd519c9fa4f92279d24a697269ebd1641b0
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 17:18:49 +0200

    7_7

---
+++ 7_Les_Generics_Utility_Types/7_7_G_Pick.ts
@@ -0,0 +1,27 @@
+// interface Todo {
+//     title: string;
+//     description: string;
+//     completed: boolean;
+// }
+
+// let todo1: Todo = {
+//     title: "Sport",
+//     description: "Footing",
+//     completed: true
+// }
+
+// type TodoPreview = Pick<Todo, 'title' |'completed'>
+
+// let todoPreview1: TodoPreview = {
+//     title: "Cooking",
+//     completed: false
+// }
+
+
+// // let todo2: Pick<Todo, "title" | "completed"> = {
+// //     title: "Cooking",
+// //     completed: true
+// // }
+
+// console.log(todo1);
+// console.log(todoPreview1);
