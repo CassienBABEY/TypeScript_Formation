5540c4b2ad9c4761b036b865f01edc1d70f7b834
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 16:57:38 +0200

    7_6

---
+++ 7_Les_Generics_Utility_Types/7_6_G_Records.ts
@@ -0,0 +1,24 @@
+// interface SuperHeros {
+//     name: string;
+//     power: number;
+//     marvel: boolean;
+//     dcComics: boolean;
+// }
+
+// type Name = "Batman" | "Spiderman" | "Superman"
+
+// // Record
+// let heros: Record<Name, SuperHeros> = {
+//     Batman: {name: "Batman", power: 60, marvel: false, dcComics: true},
+//     Spiderman: {name: "Spiderman", power: 60, marvel: true, dcComics: false},
+//     Superman: {name: "Superman", power: 100, marvel: false, dcComics: true}    
+// }
+
+// console.log(heros);
+
+// heros.Spiderman.marvel && console.log(`${heros.Spiderman.name} is Marvel!`);
+
+// heros = {Batman: {name: "Batman", power: 60, marvel: false, dcComics:true},
+// Spiderman: {name: "Spiderman", power: 60, marvel: true, dcComics: false},
+//     Superman: {name: "Superman", power: 100, marvel: false, dcComics: true}}
+
