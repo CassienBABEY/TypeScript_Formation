e831a072ff3ec7dbea9a9cee5adf1dea6fab4b37
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 16:25:37 +0200

    7_4

---
+++ 7_Les_Generics_Utility_Types/7_4_G_Partial.ts
@@ -0,0 +1,25 @@
+// interface Todo {
+//     title:string;
+//     description:string;
+// }
+
+// function addTodo(title:string, description:string): Todo {
+//     let myTodo: Partial<Todo> = {};
+//     myTodo.title = title;
+//     myTodo.description = description;
+
+//     return <Todo>myTodo //as Todo;
+// }
+
+// const todo = addTodo("Sport", "Faire un footing"); 
+// console.log(todo);
+
+
+// function addTodo2(title:string, description:string): Todo {
+//     let myTodo: Todo = {title, description};
+   
+//     return myTodo //as Todo;
+// }
+
+// const todo2 = addTodo("Sport", "Faire un footing"); 
+// console.log(todo2);
