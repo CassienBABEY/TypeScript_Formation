f2368174b2df7f5c9a4e5216e1ff63433c675ee6
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 21:12:22 +0200

    10_11

---
+++ 7_Les_Generics_Utility_Types/7_11_G_NonNulalble.js
@@ -0,0 +1,9 @@
+"use strict";
+// class Car {
+//     color?: "Green" | "Red" | "Blue" | "Yellow";
+//     paint(color: NonNullable<Car['color']>) {
+//         console.log(color);
+//     }
+// }
+// const ford = new Car();
+// ford.paint(undefined)
---
+++ 7_Les_Generics_Utility_Types/7_11_G_NonNulalble.ts
@@ -0,0 +1,12 @@
+// class Car {
+//     color?: "Green" | "Red" | "Blue" | "Yellow";
+    
+
+//     paint(color: NonNullable<Car['color']>) {
+//         console.log(color);
+//     }
+// }
+
+
+// const ford = new Car();
+// ford.paint(undefined)
