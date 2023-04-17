b513d42d6e1c1179738618a02741f8617e4e6e5c
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 18:18:46 +0200

    7_10'

---
+++ 7_Les_Generics_Utility_Types/7_10_G_Extract.js
@@ -0,0 +1,10 @@
+"use strict";
+// type Colors = "blue" | "red" | "green" | "yellow" 
+// type MyColors = "red" | "purple" | "blue"
+// type AcceptedColors= Extract<Colors, MyColors> // red blue 
+// function clgColors(colors: AcceptedColors): Colors {
+//     console.log(colors);
+// }
+// clgColors("red"); // ok
+// clgColors("blue"); // ok 
+// clgColors("green"); // type non accepté
