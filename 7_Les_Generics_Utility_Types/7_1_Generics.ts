71cae4ce8bcf6e0ac1fd9542535f9dcc7221f549
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 14:09:16 +0200

    7-1

---
+++ 7_Les_Generics_Utility_Types/7_1_Generics.ts
@@ -0,0 +1,35 @@
+// Generics
+
+// let colors = ["red","blue","green"]; // string[]
+
+// let colors2 = []; // any[]
+
+// let colors3: Array<string> = []; // string[]
+
+// // colors3 = ["red","blue",200] // Erreur numbre != string
+
+// function identity<T extends {title: string}>(arg:T): T {
+//     return arg;
+// }
+
+// let output1 = identity<string>("Hello"); // string
+// let output2 = identity(200); // number
+// let output3 = identity(true); // boolean
+// let output4 = identity({title: "HelloWorld"}) // {title: string}
+
+// console.log(typeof output1); // Hello;
+// console.log(typeof output2); // Hello);
+// console.log(typeof output3); // Hello);
+// console.log(typeof output4); // Hello);
+
+
+// const objOne = {name:"Bart"}; 
+// const objTwo = {age:8};
+
+// function objInArray<N extends {name: string}, A extends {age: number}>(n: N, a: A) {
+//     let data: Array<Object> = [];
+//     data.push(n, a);
+//     return data;
+// }
+
+// console.log(objInArray(objOne, objTwo));
--- script.js
+++ script.js
@@ -1,19 +1,1 @@
 "use strict";
-// Abstracts
-class Person {
-    constructor(name) {
-        this.name = name;
-    } // On ne peut pas static dans constructeur
-    walk() {
-        console.log("I walk");
-    }
-}
-class Child extends Person {
-    updateName(name) {
-        this.name = name;
-        console.log(this.name);
-    }
-}
-let Mario = new Child("Mario");
-Mario.walk();
-Mario.updateName("Maria");
