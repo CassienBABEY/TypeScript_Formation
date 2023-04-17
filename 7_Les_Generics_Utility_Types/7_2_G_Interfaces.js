8e3f0a6f91b3a6d2bd3dd7e2c4ef312f15a657bc
CassienBABEY <cassienbabey@gmail.com>
2023-04-17 16:05:15 +0200

    7_3

---
+++ 7_Les_Generics_Utility_Types/7_2_G_Interfaces.js
@@ -0,0 +1,14 @@
+"use strict";
+// interface User<T> {
+//     id: number;
+//     data: T;
+// }
+// const userData: User<string[]> = {
+//     id: 6845153,
+//     data: ["Hello", "World"]
+// }
+// const userData2: User<{name:string, role:string}> = {
+//     id: 6845153,
+//     data: {name: "Wario", role: "admin"}
+// }
+// console.log(userData2);
---
+++ 7_Les_Generics_Utility_Types/7_3_G_Classes.ts
@@ -0,0 +1,22 @@
+// // Mission : Enregister type string
+
+
+
+// class DataBase<T extends string |number> { // Génère une contrainte de type
+//     data:Array<T> = [];
+
+//     savedData(val:T) {
+//         this.data.push(val);
+//     }
+
+//     getAllData() {
+//         return [... this.data]
+//     }
+// }
+
+// const stringData = new DataBase<string | number | object>(); // DataBase
+// stringData.savedData("Hello");
+// stringData.savedData("Prout");
+// stringData.savedData(200);
+// stringData.savedData({ name: "John", age: 20 });
+// console.log(stringData.getAllData());
