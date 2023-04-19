02afdb588ec9e0008677f56d0f71c4b185ceac94
CassienBABEY <cassienbabey@gmail.com>
2023-04-19 21:42:56 +0200

    Part1 Facturier

--- 7_Les_Generics_Utility_Types/7_13_G_ReturnType.js
+++ 7_Les_Generics_Utility_Types/7_13_G_ReturnType.js
@@ -1,14 +1,16 @@
 "use strict";
-function fetchUser(id, username) {
-    return {
-        id,
-        username
-    };
-}
-function fetchLoggedInUser(...params) {
-    const memberData = fetchUser(...params);
-    console.log(memberData);
-    let user = memberData;
-    console.log(user);
-}
-fetchLoggedInUser(3, "Wario"); // OK
+// function fetchUser(id: number, username: string) {
+//     return {
+//         id,
+//         username
+//     }
+// }
+// type FetchUserParams = Parameters<typeof fetchUser>;
+// type FetchUserReturnType = ReturnType<typeof fetchUser>; // Object
+// function fetchLoggedInUser(...params: FetchUserParams) {
+//     const memberData = fetchUser(...params);
+//     console.log(memberData);
+//     let user: FetchUserReturnType = memberData;
+//     console.log(user);
+// }
+// fetchLoggedInUser(3, "Wario"); // OK
