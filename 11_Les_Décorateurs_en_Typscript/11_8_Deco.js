b59c09e58c5e5858818609dd00d3bae4f3230cf8
CassienBABEY <cassienbabey@gmail.com>
2023-04-25 20:33:08 +0200

    11_8_Deco

---
+++ 11_Les_Décorateurs_en_Typscript/11_8_Deco.js
@@ -0,0 +1,26 @@
+"use strict";
+// function changeArtist(artist: string){
+//     return function(constructor: any) {
+//         return class {
+//             public age:number; 
+//             public eyes:string;
+//             public artist: string = artist;
+//             loginMsg(){
+//                 return "Hello"
+//             }
+//         }
+//     }
+// }
+// @changeArtist("John Cena")
+// class Person {
+//     constructor(public age:number, public eyes:string, public artist: string) {}
+//     loginMsg(minAge:number, textOne: string, textTwo: string){
+//         if(this.age > 17){
+//             return textOne;
+//         }
+//         return textTwo;
+//     }
+// }
+// const person = new Person(19, "blue", "Picasso");
+// console.log(person.artist);
+// console.log(person.loginMsg(17, 'bleu',' Picasso'));
