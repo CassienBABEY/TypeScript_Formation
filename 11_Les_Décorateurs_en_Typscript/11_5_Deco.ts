e85a022735e773211f10890269e5dae5e8748509
CassienBABEY <cassienbabey@gmail.com>
2023-04-25 14:55:44 +0200

    10_5_deco

---
+++ 11_Les_Décorateurs_en_Typscript/11_5_Deco.ts
@@ -0,0 +1,24 @@
+function PropertyLog(target: any, propertyKey: string) {
+    console.log(target);
+    console.log(propertyKey);
+}
+
+class Car {
+    @PropertyLog
+    brand: string;
+    color: string;
+
+
+    constructor(b: string, c: string) {
+        this.brand = b;
+        this.color = c;
+
+    }
+
+    getInfos() {
+        return `Marque: ${this.brand} | Couleur: ${this.color}`;
+    }
+}
+
+const opel = new Car("Opel", "bleu");
+console.log(opel);
